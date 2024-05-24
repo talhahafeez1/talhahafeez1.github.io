bd7fa7f096461f1b2c48c2a76b289daf5c40edcc
Talha <talhahafeez03@gmail.com>
2024-05-24 06:19:26 -0400

    Removed Login.js

--- fs/script.js
+++ fs/script.js
@@ -1,8 +1,4 @@
-import 'https://tomashubelbauer.github.io/github-pages-local-storage/index.js';
-
 const date = new Date();
-console.log(localStorage.getItem("color"));
-document.getElementsByClassName("month")[0].style.backgroundColor = localStorage.getItem("color");
 
 const novMatchDays = new Set([ // NOVEMBER hardcoded set of exhibition match days
   
@@ -246,3 +242,36 @@ function objLength(obj) {
 
 renderCalendar()
 
+function loginUser(){
+  username = document.getElementById("inputUser").value;
+  password = document.getElementById("inputPassword").value;
+  if (username != ""){
+      fetch('https://oebcalendar-c34e0-default-rtdb.firebaseio.com/posts.json?AIzaSyBKQ7SbuDkeqsN8d22tAC_a52kpwaKSJVA')
+      .then(res => res.json())
+      .then(async data => {
+          let databaseUserInfo = data["users"][username]; // point to the day clicked
+          
+          // Check if user exists
+          if (databaseUserInfo == null){
+              alert("User does not exist!");
+              return;
+          } 
+          
+          // Check if password matches database saved password
+          if (databaseUserInfo['pass'] != password){
+              alert("Password is not correct! Please try again!");
+              return;
+          }
+
+          // Check if user is an admin
+          if (databaseUserInfo['admin']){
+              localStorage.setItem("color", "red");
+          }
+          window.location.href = "https://talhahafeez1.github.io/calendar.html";
+      });
+  }
+}
+
+function createAcc() {
+  window.location.href = "https://talhahafeez1.github.io/register.html";
+}
