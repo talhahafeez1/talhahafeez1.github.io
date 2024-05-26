f2b0d1f8ff77ac98b36e9e6bb3cee322462c39f5
Talha <talhahafeez03@gmail.com>
2024-05-25 21:06:03 -0400

    Testing Database

--- fs/login.js
+++ fs/login.js
@@ -12,6 +12,21 @@
 // });
 
 // import firebase from "firebase";
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    // ...
+    // The value of `databaseURL` depends on the location of the database
+    databaseURL: 'https://oebcalendar-c34e0-default-rtdb.firebaseio.com',
+  };
+  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+  
+  
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
 
 window.loginUser = function (){
     var username = document.getElementById("inputUser").value;
