a4f477dd4c433e081465fbadfa2db0f22e48821c
Talha <talhahafeez03@gmail.com>
2024-05-25 20:59:06 -0400

    Finished register, changed type to module again

--- fs/login.js
+++ fs/login.js
@@ -14,8 +14,8 @@
 // import firebase from "firebase";
 
 window.loginUser = function (){
-    username = document.getElementById("inputUser").value;
-    password = document.getElementById("inputPassword").value;
+    var username = document.getElementById("inputUser").value;
+    var password = document.getElementById("inputPassword").value;
     if (username != ""){
         fetch('https://oebcalendar-c34e0-default-rtdb.firebaseio.com/posts.json?AIzaSyBKQ7SbuDkeqsN8d22tAC_a52kpwaKSJVA')
         .then(res => res.json())
